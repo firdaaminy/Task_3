678fc310a93e8fbe6db2d4623edc097a1ca9da6c
FIRDA AMINY MA'RUF <firda_maruf@yahoo.com>
2015-09-17 00:38:38 +0700

    Final Commit

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include "list.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int pilihan;
+ //   list L;
+   // (L).first=nil;
+        ShowMenu(pilihan);
+
     return 0;
 }
